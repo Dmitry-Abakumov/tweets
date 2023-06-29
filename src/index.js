f317572acbef791900d632f2a5e390e45c8336fb
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-06-29 18:26:01 +0200

    renames homepage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets-app">
+    <BrowserRouter basename="/tweets">
       <App />
       <Toaster position="top-right" />
     </BrowserRouter>
