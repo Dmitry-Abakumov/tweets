f1665aac50b96c5ded6eaa178a5a2ab6f3367f61
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-06-30 11:16:37 +0200

    changes basename

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/tweets-app">
+    <BrowserRouter>
       <App />
       <Toaster position="top-right" />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
